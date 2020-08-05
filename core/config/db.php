af47b7580a0aee5696f6f1abcdbc1f54d273361e
lheokk <lheokk@gmail.com>
2020-08-05 22:21:38 +0200

    1
    engine

---
+++ core/config/db.php
@@ -0,0 +1,42 @@
+<?php
+
+    /**
+     * Конфиг работы с БД
+     *
+     * @author Zmi
+     */
+
+
+    $g_config['dbSimple']                     = array();
+    $g_config['dbSimple']['logDbError']       = true;
+    $g_config['dbSimple']['dbLogFile']        = BASEPATH . 'tmp/log_db.txt';
+
+    // Имена БД объектов, будут хранится в $g_databases->dbName подключение к БД задается через DSN
+    // Работа с БД происходит при помощи DbSimple (http://dklab.ru/lib/DbSimple/)
+    $g_config['dbSimple']['databases'] = array
+    (
+    );
+    
+    if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1')
+    {
+        $g_config['pdo']['databases'] = array
+        (
+            'db' => array(
+                'dsn' => 'mysql:host=localhost;dbname=bitarel;charset=utf8',
+                'login' => 'root',
+                'pwd' => ''
+            ),
+        );
+    }
+    else
+    {
+        $g_config['pdo']['databases'] = array
+        (
+            'db' => array(
+                'dsn' => 'mysql:host=localhost;dbname=ca31197_bitarel;charset=utf8',
+                'login' => 'ca31197_bitarel',
+                'pwd' => 'gfsUsj200sj2Jhs'
+            ),
+        );
+    }
+?>
