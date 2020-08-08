3d35038211d7e2c047d0981dca9e1b0fa6e8fb43
lheokk <lheokk@gmail.com>
2020-08-08 16:30:21 +0200

    Update db_and_models.php

--- core/init/db_and_models.php
+++ core/init/db_and_models.php
@@ -6,11 +6,13 @@
      * @author Zmi
      */
 
-
     // Стартуем все БД
-    require_once BASEPATH . 'lib/Db/Db.php';
-    new Db();
+//    require_once BASEPATH . 'lib/Db/Db.php';
+//    new Db();
     // Теперь должна быть переменная $g_databases где лежат все БД
+
+    require_once BASEPATH . 'lib/Db/Db_pdo.php';
+    new Db_pdo();
 
     // Список подключаемых моделей
     require_once BASEPATH . 'model/Model.php';
